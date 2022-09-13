37d9bbd79c624c3b891351615557c4f2e361041b
ojthesixth <110111400+ojthesixth@users.noreply.github.com>
2022-09-13 12:33:05 +0200

    tutan soulz08
    replaced placeholder with player, corrected its size and animation.

--- Assets/player_mouvement.cs
+++ Assets/player_mouvement.cs
@@ -8,10 +8,13 @@ public class player_mouvement : MonoBehaviour
     public float _speed = 7f;
     public GameObject player;
     public Rigidbody2D _rigidbody;
-    public BoxCollider2D _collider2D;
+    //public BoxCollider2D _collider2D; <-was for square placeholder
+    public CapsuleCollider2D _collider2D;
+
 
     public Vector3 direction;
 
+    //public Vector2 velocity = new Vector2();
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +28,14 @@ public class player_mouvement : MonoBehaviour
         /*
         playerMove = Input.GetAxisRaw("Horizontal");
         playerMove = Input.GetAxisRaw("Vertical");
-        _rigidbody.velocity = new Vector3(playerMove * _speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector3(playerMove * _speed, _rigidbody.velocity.y.x);
         */
 
-        
+
         float v = Input.GetAxis("Vertical") * _speed;
         float r = Input.GetAxis("Horizontal") * _speed;
         _rigidbody.velocity = new Vector3(r, 0, v);
-        
+
 
         /*
         if (Input.GetButton("Horizontal"))
@@ -41,7 +44,21 @@ public class player_mouvement : MonoBehaviour
             GameObject.transform.translate(new Vector3(-0.01f, 0, 0));
         }
 
-        (?°?°??? ??? AGH!!!
-        */
+        AGH!!!
+
+           if (Input.IsActionPressed("right"));
+        if (Input.IsActionPressed("left"));
+        if (Input.IsActionPressed("down"))
+            velocity = new Vector2(-speed, 0);
+        if (Input.IsActionPressed("up"))
+            velocity = new Vector2(speed, 0);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }   */
+
     }
 }
